6e231c6f2e423ce8f53259fad51540e71459c409
miskang863 <miskanghipandulwa@ymail.com>
2021-02-09 11:07:38 +0200

    Bottom navigation added

--- app/src/main/java/com/example/birder/MainActivity.kt
+++ app/src/main/java/com/example/birder/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.birder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.example.birder.fragments.FavoritesFragment
+import com.example.birder.fragments.HomeFragment
+import com.example.birder.fragments.MapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val favoritesFragment = FavoritesFragment()
+        val mapFragment = MapFragment()
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_favorite -> makeCurrentFragment(favoritesFragment)
+                R.id.ic_map -> makeCurrentFragment(mapFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/birder/fragments/MapFragment.kt
@@ -0,0 +1,26 @@
+package com.example.birder.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.birder.R
+
+class MapFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+
+}
